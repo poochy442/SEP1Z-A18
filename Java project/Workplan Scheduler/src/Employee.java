32178f4d25515c60220d7f2e638320787a99eccf
poochy442 <kenneth.velling@hotmail.com>
2018-12-09 13:49:33 +0100

    Employee finished

--- Java project/Workplan Scheduler/src/Employee.java
+++ Java project/Workplan Scheduler/src/Employee.java
@@ -6,11 +6,13 @@ public class Employee extends Person {
 
 	// Fields
 	List<SimpleEntry<String, Integer>> preferences;
+	List<SimpleEntry<String, Integer>> training;
 
 	// Constructor
 	public Employee(String name, String initials, int day, int month, int year) {
 		super(name, initials, day, month, year);
 		preferences = new ArrayList<>();
+		training = new ArrayList<>();
 	}
 
 	// Methods
@@ -34,5 +36,26 @@ public class Employee extends Person {
 			}
 		}
 		return 0;
+	}
+	
+	public List<SimpleEntry<String, Integer>> getTraining(){
+		return training;
+	}
+	
+	public void setTraining(List<SimpleEntry<String, Integer>> training) {
+		this.training = training;
+	}
+	
+	public void addTraining(String test, int level) {
+		training.add(new SimpleEntry<String, Integer>(test, level));
+	}
+	
+	public int checkForTraining(String test) {
+		for(SimpleEntry<String, Integer> entry : training) {
+			if(entry.getKey().equals(test)) {
+				return entry.getValue();
+			}
+		}
+		return -1;
 	}
 }
