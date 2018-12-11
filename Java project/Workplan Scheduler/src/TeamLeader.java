0ba2a6297181584d07209d60c44740977a1123ad
Teakosheen <43374385+Teakosheen@users.noreply.github.com>
2018-12-12 00:47:13 +0100

    TeamLeader2

--- Java project/Workplan Scheduler/src/TeamLeader.java
+++ Java project/Workplan Scheduler/src/TeamLeader.java
@@ -1,11 +1,48 @@
+import java.util.*;
 
 public class TeamLeader extends Person {
 
-	// TODO: create login
-	
-	public TeamLeader(String name, String initials, int day, int month, int year) {
+	private String userName;
+	private String password;
+	private String teamName;
+
+	// constructor
+	public TeamLeader(String name, String initials, int day, int month, int year, String userName, String password) {
 		super(name, initials, day, month, year);
-		// TODO Auto-generated constructor stub
+		this.userName = userName;
+		this.password = password;
+	}
+
+	// Methods
+	public String getUserName() {
+		return userName;
+	}
+
+	// set the userName
+	public void setUserName(String initials) {
+		userName = initials;
+	}
+
+	// create the password
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	// test userName and Password
+	public void checkLogin() {
+		Scanner scan = new Scanner(System.in);
+		String userName = scan.nextLine();
+	    String password1 = scan.nextLine();
+		
+		if(userName.equals(getInitials()) && password1.equals(password));
+		{
+			System.out.println("You are logged in ");
+		}
+		else
+		{
+		System.out.println("Incorrect user name or password");
+		}
+		
 	}
 
 }
