fda9789227708eda535541ae430ee54ced9bea5e
poochy442 <kenneth.velling@hotmail.com>
2018-12-12 11:00:57 +0100

    Update TeamLeader.java

--- Java project/Workplan Scheduler/src/TeamLeader.java
+++ Java project/Workplan Scheduler/src/TeamLeader.java
@@ -1,11 +1,55 @@
+import java.util.*;
 
 public class TeamLeader extends Person {
 
-	// TODO: create login
-	
-	public TeamLeader(String name, String initials, int day, int month, int year) {
+	// Fields
+	private String userName;
+	private String password;
+	private String teamName;
+
+	// constructor
+	public TeamLeader(String name, String initials, int day, int month, int year, String userName, String password) {
 		super(name, initials, day, month, year);
-		// TODO Auto-generated constructor stub
+
 	}
 
+	// Methods
+	public String execute() {
+		if (this.userName.equals("admin") && this.password.equals("admin123")) {
+			return "success";
+		} else {
+			return "error";
+		}
+	}
+
+	// Getters and Setters
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getTeamName() {
+		return teamName;
+	}
+
+	public void setTeamName(String teamName) {
+		this.teamName = teamName;
+	}
+
+	/*
+	 * public static void main(String[] args) { TeamLeader Teako = new
+	 * TeamLeader("Teakosheen", "TK", 1, 7, 1983); Teako.setPassword("123");
+	 * System.out.println(Teako.getPassword()); }
+	 */
 }
