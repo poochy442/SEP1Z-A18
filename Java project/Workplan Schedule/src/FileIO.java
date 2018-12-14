d1688498581b8bd1be133a450edd366f08ec6b1a
poochy442 <kenneth.velling@hotmail.com>
2018-12-14 12:43:22 +0100

    File IO

--- Java project/Workplan Schedule/src/FileIO.java
+++ Java project/Workplan Schedule/src/FileIO.java
@@ -1,8 +1,50 @@
+import java.io.*;
 
 public class FileIO {
-	
+
+	private static String fileName;
+
 	public static void saveToBin(Object obj) {
-		
+		ObjectOutputStream out = null;
+
+		try {
+			File file = new File(fileName);
+			FileOutputStream fos = new FileOutputStream(file);
+			out = new ObjectOutputStream(fos);
+
+			out.writeObject(obj);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				out.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
-	
+
+	public static Object loadFromBin() {
+		ObjectInputStream in = null;
+		Object obj = null;
+		
+		try {
+			File file = new File(fileName);
+			FileInputStream fis = new FileInputStream(file);
+			in = new ObjectInputStream(fis);
+			
+			obj = (Object) in.readObject();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				in.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return obj;
+	}
+
 }
